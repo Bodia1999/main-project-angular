d3e998cc6bdadf2b52b1c6d1ee2452b6f7c175a9
Bodia1999 <92782560bodia>
2019-08-08 23:45:44 +0300

    updates1

--- src/app/pipes/revert-time.pipe.ts
+++ src/app/pipes/revert-time.pipe.ts
@@ -6,9 +6,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class RevertTimePipe implements PipeTransform {
 
   updatedValue: string;
+  time: string;
   transform(value: string): any {
+
+    if (value === null) {
+      return;
+    }
+    if (value.indexOf('.') === 19) {
+      this.time = value.substring(value.indexOf('T') + 1, value.indexOf('.'));
+    } else {
+      this.time = value.substring(value.indexOf('T') + 1, 25);
+    }
     this.updatedValue = value.split('T')[0];
-    return this.updatedValue;
+
+    return this.updatedValue.concat(' ').concat(this.time);
   }
 
 }
